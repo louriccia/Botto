f4e9c67c404fb7088e105c318c105d3ef1eced60
louriccia <louriccia@gmail.com>
2020-08-31 17:49:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ if(messageLow.startsWith(`${prefix}tourn`)){
     if (numb !== null) {
         const tourneyfiltered = tourney.filter((value, key) => key == tracks[numb].name)
         for (i=0; i<5; i++){
-            message.channel.send(tourney[i].player + " - " + timefix(tourney[i].totaltime))
+            message.channel.send(tourneyfiltered[i].player + " - " + timefix(tourneyfiltered[i].totaltime))
         }
     }
 }
