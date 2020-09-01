74e79ebd53a26c0793cb4e9892c360b65cdd94e3
louriccia <louriccia@gmail.com>
2020-09-01 13:58:23 -0700

    fixed year

--- index.js
+++ index.js
@@ -601,7 +601,7 @@ if(messageLow.startsWith(`${prefix}tourn`)){
                         }
                     } 
                     tourneyReport.addField(
-                        pos[i] + " " + tourneyfiltered[j].player, "2019, " + tourneyfiltered[j].bracket +": "+tourneyfiltered[j].round + "\nRace " + tourneyfiltered[j].race + ", vs " + tourneyfiltered[j].opponent, true
+                        pos[i] + " " + tourneyfiltered[j].player, tourneyfiltered[j].year + ", " + tourneyfiltered[j].bracket +": "+tourneyfiltered[j].round + "\nRace " + tourneyfiltered[j].race + ", vs " + tourneyfiltered[j].opponent, true
                     )
                     tourneyReport.addField(
                         timefix(tourneyfiltered[j].totaltime)," " + character + "[ " + forc + "](" + link + ")" + deaths + characterban, true
