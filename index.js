2625c7496cecf36209a57170af564decf453cb75
louriccia <louriccia@gmail.com>
2020-10-31 15:36:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1786,7 +1786,7 @@ if (!message.guild) return; //anything that shouldn't be called in a dm goes aft
 if(message.content.startsWith(`${prefix}speedrunning`)){
     let role = message.guild.roles.cache.get("535973118578130954");
     let member = message.member;
-    if(message.member.roles.find(r => r.name === "Speedrunning")){
+    if(message.member.roles.cache.find(r => r.name === "Speedrunning")){
         member.roles.remove(role).catch(console.error)
         message.channel.send(member + " no longer has the speedrunning role")
     } else {
@@ -1798,7 +1798,7 @@ if(message.content.startsWith(`${prefix}speedrunning`)){
 if(message.content.startsWith(`${prefix}multiplayer`)){
     let role = message.guild.roles.cache.get("474920988790751232");
     let member = message.member;
-    if(message.member.roles.find(r => r.name === "Multiplayer")){
+    if(message.member.roles.cache.find(r => r.name === "Multiplayer")){
         member.roles.remove(role).catch(console.error)
         message.channel.send(member + " no longer has the multiplayer role")
     } else {
