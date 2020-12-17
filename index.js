1ed50612968922011c4cbf24aeddde7f1154e5ea
louriccia <louriccia@gmail.com>
2020-12-16 17:40:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1318,8 +1318,8 @@ if(message.channel.id == 545800310283829270) { //775134898633048084 weekly chall
     if (att.length > 0) {
         url = att[0].url
     }
-    if (emb[0].url !== "") {
-        url = emb[0].url
+    if (emb[0].video.url !== "") {
+        url = emb[0].video.url
         embtitle = emb[0].title
     }
 
