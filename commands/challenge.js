f51d9cb15c4eeea08fcb5f6cf7066200f9a028f0
louriccia <louriccia@gmail.com>
2020-12-31 01:22:34 -0800

    Update challenge.js

--- commands/challenge.js
+++ commands/challenge.js
@@ -3,6 +3,27 @@ module.exports = {
     execute(client, interaction, args) {
         const Discord = require('discord.js');
         var tools = require('./../tools.js');
+        var firebase = require("firebase/app");
+        require('firebase/auth');
+        require('firebase/database');
+        var firebaseConfig = {
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID,
+            measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        };
+        firebase.initializeApp(firebaseConfig);
+        var database = firebase.database();
+        var ref = database.ref('times');
+        ref.on("value", function(snapshot) {
+            challengedata = snapshot.val();
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
         //const myEmbed = new Discord.MessageEmbed()
         if(args[0].name =="generate") {
             let member = interaction.member.user.id
