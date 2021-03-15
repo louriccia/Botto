87629421d552f6e13274d7ab2815b7076fe4997a
louriccia <louriccia@gmail.com>
2021-03-15 01:16:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ var admin = require('firebase-admin');
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        "projectId": process.env.project_id,
-        "privateKey": process.env.private_key.replace(/\\n/g, '\n'),
-        "clientEmail": process.env.client_email
+        "projectId": process.env.FIREBASE_PROJECT_ID,
+        "privateKey": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "clientEmail": process.env.FIREBASE_CLIENT_EMAIL
     }),
     databaseURL: "https://botto-efbfd.firebaseio.com"
 })
