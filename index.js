98154093d0010a157bfbe7a7b794bc52df584df9
lightningpirate <louriccia@gmail.com>
2021-05-27 16:26:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ client.once('ready', () => {
             }
         }
     });
-
+/*
     var msg = new Discord.Message(client, {
         content: "This is a test message",
         components: [
@@ -196,7 +196,7 @@ client.once('ready', () => {
             }
         ]
     })
-    //client.channels.cache.get("545800310283829270").send(msg)
+    //client.channels.cache.get("545800310283829270").send(msg)*/
 
     const testEmbed = new Discord.MessageEmbed()
         .setTitle("Test")
