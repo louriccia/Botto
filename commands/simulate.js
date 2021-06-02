95f48e5df81bd82a5363a9781d9e493364ffcfba
kingbeandip <mitch.t.howell@gmail.com>
2021-06-01 20:53:46 -0600

    Update simulate.js

--- commands/simulate.js
+++ commands/simulate.js
@@ -4,7 +4,7 @@ module.exports = {
         const Discord = require('discord.js');
         var tools = require('./../tools.js');
         const myEmbed = new Discord.MessageEmbed()
-        var output = tools.simulateSpeed()
+        var output = tools.simulateSpeed().toString + ":blue_square:"
         client.api.interactions(interaction.id, interaction.token).callback.post({
             data: {
                 type: 4,
