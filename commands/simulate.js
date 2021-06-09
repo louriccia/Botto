f9c36caa10b6242fa62cd7732644cb1e988d5772
kingbeandip <mitch.t.howell@gmail.com>
2021-06-08 22:14:20 -0600

    descrip

--- commands/simulate.js
+++ commands/simulate.js
@@ -25,18 +25,18 @@ module.exports = {
         var postColumn3 = ""
         var output = tools.simulateSpeed(track, fps, upgrades) //format:: [[pod index, average speed, finish time], [next pod], ... [final pod], fps, track]
         for (let i = 0; i < 23; i++) {
-            postColumn1 += racers[output[i][0]].name + "\n"
+            postColumn1 += racers[output[i][0]].flag + " " + racers[output[i][0]].name + "\n"
             postColumn2 += Math.round(output[i][1]*10)/10 + "\n"
             postColumn3 += tools.timefix(output[i][2]) + "\n"
         }
 
         myEmbed
-            .setTitle(tracks[track].name + " simulation") //track
-            .setDescription(upgradeName[upgrades] + ", " + fps + "fps") //upgrades & fps //"Max Upgrades, 60fps"
+            .setTitle(tracks[track].name) //track
+            .setDescription("Simulation using " + upgradeName[upgrades] + " at " + fps + "fps") //upgrades & fps //"Max Upgrades, 60fps"
             .addField("Racer", postColumn1, true)
             .addField("Speed", postColumn2, true)
             .addField("Est. Time", postColumn3, true)
-            .addField("Note:", "*This simulation assumes the track is a straight line, and ignores hills and fast/slow terrain.*", false)
+            .addField("Note:", "*This simulation assumes the track is a straight line, and ignores hills and fast/slow terrain. The estimated times might also be off by as much as 0.1s from pod to pod.*", false)
 
         
 
