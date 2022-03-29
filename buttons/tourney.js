062a2b4e4905671298a56b85ac4d061a5e83be5d
lightningpirate <louriccia@gmail.com>
2022-03-29 02:44:45 -0700

    Update tourney.js

--- buttons/tourney.js
+++ buttons/tourney.js
@@ -5311,6 +5311,14 @@ module.exports = {
                 return [tourneyReport, components]
             }).then((embed) => sendResponse(embed))
         } else if (args[0] == "play") {
+            function getPlayer (id) {
+                var tpd = Object.keys(tourney_participants_data)
+                tpd.forEach(key => {
+                    if(tourney_participants_data[key].id == id){
+                        return key
+                    }
+                })
+            }
             var type = 4
             if ([null, undefined, ""].includes(tourney_live_data[interaction.channel_id])) {
                 args[1] = "setup"
@@ -5356,7 +5364,8 @@ module.exports = {
                     .setTitle("Match Setup")
                     .setDescription("Tournament: `" + (tourney_live_data[interaction.channel_id].tourney == "" ? "" : tourney_tournaments_data[tourney_live_data[interaction.channel_id].tourney].name) + "`\n" +
                         "Bracket/Round: `" + (tourney_live_data[interaction.channel_id].bracket == "" ? "" : tourney_tournaments_data[tourney_live_data[interaction.channel_id].tourney].stages[tourney_live_data[interaction.channel_id].bracket].bracket + " " + tourney_tournaments_data[tourney_live_data[interaction.channel_id].tourney].stages[tourney_live_data[interaction.channel_id].bracket].round) + "`\n" +
-                        "Ruleset: `" + (tourney_live_data[interaction.channel_id].ruleset == "" ? "" : tourney_rulesets_data.saved[tourney_live_data[interaction.channel_id].ruleset].name))
+                        "Ruleset: `" + (tourney_live_data[interaction.channel_id].ruleset == "" ? "" : tourney_rulesets_data.saved[tourney_live_data[interaction.channel_id].ruleset].name)) + "`\n" + 
+                        "Players: " (tourney_live_data[interaction.channel_id].players ? "" : Object.values(tourney_live_data[interaction.channel_id].players).map(id=> "`" + tourney_participants_data[getPlayer(id)].name + "` ") )
                     .setColor("#3BA55D")
                     .setAuthor("Tournaments", "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/trophy_1f3c6.png")
                 var components = []
