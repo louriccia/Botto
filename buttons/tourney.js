941f3324dee092a72b1dfa5bf3c872008c338882
lightningpirate <louriccia@gmail.com>
2022-04-08 00:06:09 -0700

    Update tourney.js

--- buttons/tourney.js
+++ buttons/tourney.js
@@ -5766,7 +5766,7 @@ module.exports = {
                     data: {
                         type: type,
                         data: {
-                            content: "" + [undefined, null].includes(livematch.firstvote) ? Object.values(livematch.players).map(player => "<@" + player + ">").join(",, ") : (Object.values(livematch.players).map(player => (Object.keys(livematch.firstvote).includes(player) ? "" : "<@" + player + ">"))).join(", "),
+                            content: "" + ([undefined, null].includes(livematch.firstvote) ? Object.values(livematch.players).map(player => "<@" + player + ">").join(",, ") : (Object.values(livematch.players).map(player => (Object.keys(livematch.firstvote).includes(player) ? "" : "<@" + player + ">"))).join(", ")),
                             embeds: [firstselect],
                             components: [
                                 {
