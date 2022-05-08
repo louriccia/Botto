d640f53cec6dc095efe5cfc0ce7d6cceaa29a498
lightningpirate <louriccia@gmail.com>
2022-05-07 23:10:18 -0700

    update

--- buttons/tourney.js
+++ buttons/tourney.js
@@ -5470,14 +5470,16 @@ module.exports = {
 
             function getForcePoints(player) {
                 livematch = tourney_live_data[interaction.channel_id]
-                let forcepoints = liverules.match.forcepoints.start
+                let forcepoints = Number(liverules.match.forcepoints.start)
                 let races = Object.values(livematch.races)
                 races.forEach(race => {
                     if (![null, undefined, ""].includes(race.events)) {
                         let events = Object.values(race.events)
                         events.forEach(event => {
+                            console.log(event)
                             if (event.player == player && [null, undefined, ""].includes(event.cost)) {
-                                forcepoints -= event.cost
+                                forcepoints -= Number(event.cost)
+                                console.log(forcepoints)
                             }
                         })
                     }
