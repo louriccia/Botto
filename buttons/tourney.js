85ba991c14069c2111632f5d8da3494f251b745b
lightningpirate <louriccia@gmail.com>
2022-06-02 10:15:23 -0700

    update

--- buttons/tourney.js
+++ buttons/tourney.js
@@ -1903,9 +1903,9 @@ module.exports = {
                                         stats.players[event.player].track[event.selection].picks.push(1)
                                         already_played.push(Number(event.selection))
                                         for (let i = 0; i < 25; i++) {
-                                            if (!temptrack.includes(i) && !already_banned.includes(i) && (!already_played.includes(i) || (already_played.includes(i) && (runback[race.events.filter(e => e.type == 'track' && e.event == 'selection')[0].player] !== undefined)))) {
+                                            if (!temptrack.includes(i) && !already_banned.includes(i) && (!already_played.includes(i) || (already_played.includes(i) && (runback[event.player] !== undefined)))) {
                                                 stats.track[i].picks.push(0)
-                                                stats.players[race.track_selection.player].track[i].picks.push(0)
+                                                stats.players[event.player].track[i].picks.push(0)
                                             }
                                         }
                                     }
