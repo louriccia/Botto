4501c37a86e7f017f224ae469d809f657fca452d
lightningpirate <louriccia@gmail.com>
2022-08-01 22:08:01 -0700

    update

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ client.once('ready', () => {
         .catch(console.error);
     client.channels.cache.get("444208252541075476").send("Deployed <t:" + Math.round(Date.now() / 1000) + ":R>");
     try {
-        client.commands.get("scrape").execute();
+        client.commands.get("scrape").execute(client);
     } catch {
         console.error(error);
     }
