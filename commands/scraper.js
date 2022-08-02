3661fc3cdbe4fb4ba4414e1b08b5dc297f81daa6
lightningpirate <louriccia@gmail.com>
2022-08-01 23:56:52 -0700

    update

--- commands/scraper.js
+++ commands/scraper.js
@@ -40,9 +40,7 @@ module.exports = {
                                         })
                                     })
                                 }
-                            }).catch((err) => {
-                                throw err;
-                            });
+                            })
                         })
                     } catch (e) {
                         console.log(e)
