6e5420d2a2ea700211b6043efb1f77e4349e1c98
lightningpirate <louriccia@gmail.com>
2023-01-29 20:56:30 -0800

    Update simulate.js

--- commands/simulate.js
+++ commands/simulate.js
@@ -1,4 +1,4 @@
-let { tracks } = require('../data.js')
+import { tracks } from '../data.js';
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
