1fbaeb617098f4343b453bb738b439295a69aa5d
lightningpirate <louriccia@gmail.com>
2023-02-15 01:07:24 -0800

    Update challenge.js

--- buttons/challenge.js
+++ buttons/challenge.js
@@ -100,6 +100,7 @@ module.exports = {
             if (!profile.achievements.big_time_swindler) {
                 postMessage(client, current_challenge.channel, { embeds: [achievementEmbed(name, avatar, achievement_data.big_time_swindler, current_challenge.guild)] })
                 profileref.child('achievements').child("big_time_swindler").set(true)
+                profile = userdata[player]?.random
             }
         }
 
