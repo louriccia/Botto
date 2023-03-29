918d2e920c7f6e384763a499e21e8239c058db7f
lightningpirate <louriccia@gmail.com>
2023-03-29 16:19:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ client.once(Events.ClientReady, () => {
         .catch(console.error);
     client.channels.cache.get("444208252541075476").send("Deployed <t:" + Math.round(Date.now() / 1000) + ":R>");
     try {
-        client.commands.get("scrape").execute(client, database);
+        //client.commands.get("scrape").execute(client, database);
     } catch {
         console.error("scrape failed");
     }
