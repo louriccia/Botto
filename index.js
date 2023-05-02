a01b31c2c6d88ae7fcca1382b1b1d55fe6d44695
lightningpirate <louriccia@gmail.com>
2023-05-02 15:12:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -255,13 +255,13 @@ client.once(Events.ClientReady, async () => {
                                     match.url = ""
                                 }
                             } else if (values[j].includes("comm")) {
-                                content.split(/[^A-Za-z_ ]+/g).map(comm => getParticipantbyName(comm)).filter(c => ![null, undefined, ''].includes(c)).forEach(c => {
+                                content.split(/[^A-Za-z0-9_ ]+/g).map(comm => getParticipantbyName(comm)).filter(c => ![null, undefined, ''].includes(c)).forEach(c => {
                                     match.commentators[c] = users[c].discordID ?? ''
                                 })
                             } else if (values[j].includes("date")) {
                                 match.datetime = Date.parse(content.replace(", ", " " + new Date().getFullYear() + " ").replace(" ", " ") + " EDT")
                             } else if (values[j].includes("players")) {
-                                content.split(/[^A-Za-z_ ]/g).map(play => getParticipantbyName(play)).filter(p => ![null, undefined, ''].includes(p)).forEach(p => {
+                                content.split(/[^A-Za-z0-9_ ]/g).map(play => getParticipantbyName(play)).filter(p => ![null, undefined, ''].includes(p)).forEach(p => {
                                     match.players[p] = users[p].discordID ?? ''
                                 })
                             } else {
