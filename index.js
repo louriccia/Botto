63e25535503af809e3bece13f6b6c050a623cac4
lightningpirate <louriccia@gmail.com>
2023-07-11 23:51:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -629,11 +629,6 @@ async function fetchMessageContent(message) {
 client.on(Events.MessageCreate, async function (message) {
     if (message.author.bot) return; //trumps any command from executing from a bot message
 
-    if (message.content == `${prefix}guilds`) {
-        console.log(client.guilds.cache)
-        //console.log(client.guilds.cache.get("697833083201650689"))
-    }
-
     if (message.mentions.users.has('545798436105224203')) {
         const previous = await fetchMessageContent(message);
         console.log(`${previous}\n\n${message.content}`);
@@ -655,22 +650,8 @@ client.on(Events.MessageCreate, async function (message) {
         }
     }
 
-    if (message.content.toLowerCase() == `${prefix}botto`) {
-        const Discord = require('discord.js');
-        const myEmbed = new Discord.MessageEmbed()
-            .setTitle("Botto")
-            .setFooter("/botto")
-            .setDescription("Botto is a protocol droid developed by LightningPirate#5872 for the [Star Wars Episode I: Racer Discord](https://discord.gg/BEJACxXQWz). His purpose is to enhance your Star Wars Episode I: Racer gameplay. This bot can also be found on **" + String(Number(client.guilds.cache.size) - 1) + "** other servers.")
-            .addField("Features", "Botto uses Discord's integrated slash commands feature for bots. Type forward slash ('/') to see a full list of commands including several `/lookup` and `/random` commands for SWE1R content. He is also capable of getting leaderboard data from [speedrun.com](https://www.speedrun.com/swe1r) with the `/src` command and tournament leaderboards using the `/tourney` command. Another popular feature is the `/challenge` command which calls randomly generated challenges and saves submitted times.", false)
-            .addField("Invite", "To invite Botto to your server with slash commands, [click here](https://discord.com/api/oauth2/authorize?client_id=545798436105224203&permissions=0&scope=bot%20applications.commands).", false)
-            .addField("Github", "To view Botto's github page, [click here](https://github.com/louriccia/Botto).", false)
-            .addField("Feedback", "[Request a feature](https://github.com/louriccia/Botto/discussions/3) or [give your feedback](https://github.com/louriccia/Botto/discussions/4) on using the bot by commenting on the linked discussion posts.", false)
-            .setColor("#7289DA")
-            .setThumbnail(client.user.avatarURL())
-        message.channel.send(myEmbed)
-    }
 
-    if (message.content.toLowerCase() == `${prefix}kill` && message.channelID == "444208252541075476") {
+    if (message.content.toLowerCase() == `!kill` && message.channelID == "444208252541075476") {
         message.channel.send("Come back when you got some money!")
         client.destroy()
     }
