83fae2f85b3e72daee63c29ead2202f0ab0b121f
lightningpirate <louriccia@gmail.com>
2024-05-22 17:03:38 -0700

    Update drop.js

--- src/interactions/challenge/drop.js
+++ src/interactions/challenge/drop.js
@@ -2,7 +2,7 @@ const { WhyNobodyBuy } = require('../../data/discord/emoji.js');
 const { number_with_commas } = require('../../generic.js');
 const { manageTruguts } = require('./functions.js');
 
-exports.drop = async function ({ interaction, user_profile, user_profile_ref, args, db, database } = {}) {
+exports.drop = async function ({ interaction, user_profile, user_profile_ref, args, db, database, member_id } = {}) {
 
     //already claimed
     let already_claimed = db.ch.drops ? Object.values(db.ch.drops).find(drop => drop.message == interaction.message.id) : false
